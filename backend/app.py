687f26ceb879d248d5176371e8a65009c083eaf0
rdevacc <85270154+rdevacc@users.noreply.github.com>
2024-11-12 21:41:54 +0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -72,4 +72,5 @@ def decrypt():
     return jsonify({'plaintext': plaintext})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
